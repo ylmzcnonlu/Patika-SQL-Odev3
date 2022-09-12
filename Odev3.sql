4c4ca0c05400c6d1c303de5d368b12ca37219b46
Yılmaz Can <66328002+ylmzcnonlu@users.noreply.github.com>
2022-09-12 19:22:15 +0300

    Add files via upload

---
+++ Odev3.sql
@@ -0,0 +1,8 @@
+SELECT*FROM country WHERE country LIKE 'A%a'
+
+SELECT*FROM country WHERE country LIKE '______n'
+
+SELECT title FROM film WHERE title ILIKE '%t%t%t%t'
+
+SELECT*FROM film WHERE title LIKE 'C%' AND length>90 AND rental_rate=2.99
+
